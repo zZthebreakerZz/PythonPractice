828aea6e1e551eaf3e7e14eea4a5ec15e937cd71
zZthebreakerZz <toidaidot1201@gmail.com>
2019-01-29 18:11:50 +0700

    Add more feature

---
+++ practice_flaskblog/source/flaskblog/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    SECRET_KEY = 'cd478662994f06e7cf04406ff27b8828'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'nguyenjun2018@gmail.com'
+    MAIL_PASSWORD = 'Huutuan2'
